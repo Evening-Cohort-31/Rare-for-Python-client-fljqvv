0516626b1fac09d7cde6d76425fa892687f996f2
James Freeman <jamesfreeman114@gmail.com>
2026-02-21 11:58:35 -0600

    removed duplicate imports

--- src/components/posts/AllPosts.jsx
+++ src/components/posts/AllPosts.jsx
@@ -3,9 +3,8 @@ import { useNavigate, Link } from "react-router-dom";
 import { getAllPosts } from "../../services";
 import { useCurrentUser } from "../../context/CurrentUserContext.js";
 // CHANGED: Added Link alongside useNavigate (Added on view_post_details_5)
-import { Link, useNavigate } from "react-router-dom";
 import { Loading, PageHeader, Card, Container } from "../../design";;
-import { Container, PageHeader, Loading, Card } from "../../design";
+
 
 export const AllPosts = () => {
   const { currentUser } = useCurrentUser();
